83496f8b8957b9ded104f23c396ad96baf3c1c91
libertinistic <libertinistic@gmail.com>
2016-07-18 15:21:30 -0700

    initial stab at functionality

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+
+const handle = {};
+handle['/alpha'] = alpha;
+handle['/beta']  = beta;
+
+function onRequest (request, response) {
+  var pathname = url.parse(request.url).pathname;
+
+  route(handle, pathname, response)
+}
+
+function route (handle, pathname, response) {
+  if (typeof handle[pathname] === 'function') {
+  handle[pathname](response);
+  } else {
+    console.log('No request handler found for ' + pathname);
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('404 Not found');
+    response.end();
+  }
+}
+
+function alpha () {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Alpha successfully called!');
+  response.end();
+}
+
+function beta () {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Beta successfully called!');
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
