b465de36f02734eb7e287fc475ec9cde79ceddf8
Danielle H <daheber@gmail.com>
2016-07-19 18:55:02 -0700

    random, form, and thanks routes complete

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ An http server that responds with either "hello world" or "goodbye world".
 - Available paths
   - / prints "Hello World"
   - /bye prints "Goodbye World"
+  - /random prints a random number
+  - /form takes you to a form
+  - hitting the submit button on the /form page will take you to /thanks
   - ever other URL will print "Page not found!"
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const cowsay = require('cowsay');
+const fs = require('fs');
 
 const server = http.createServer((request, response)=>{
 
@@ -14,6 +15,25 @@ const server = http.createServer((request, response)=>{
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write(cowsay.say({text : 'Goodbye world!'}));
     response.end();
+
+  } else if(request.url === '/random'){
+
+    let random = Math.floor(Math.random()*5000);
+    let message = `Here is a random number for you: ${random}`;
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(cowsay.say({text : message}));
+    response.end();
+
+  } else if(request.url === '/form'){
+
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream('./templates/form.html').pipe(response);
+
+  } else if(request.method === 'POST'){
+
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream('./templates/thanks.html').pipe(response);
 
   } else {
 
