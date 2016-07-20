154c5450489215aa60476d2ec9972065789a5905
libertinistic <libertinistic@gmail.com>
2016-07-19 18:32:16 -0700

    added test structure

---
+++ test/tests.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const url = require('url');
+const http = require('http');
+
+server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('HTTP server', () => {
+
+  before(() => {
+
+  });
+
+  it('test URL response', () => {
+
+  });
+
+  it('test query param response', () => {
+
+  });
+
+  it('test HTTP verb response', () => {
+
+  });
+
+  it('test 400 response', () => {
+
+  });
+
+  after(() => {
+
+  });
+});
